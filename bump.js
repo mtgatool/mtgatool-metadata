8fd506f340824e176223ba00d3206bc22927fd05
Manuel Etchegaray <manuel.etchegaray7@gmail.com>
2019-11-24 23:49:01 -0300

    bump test

---
+++ bump.js
@@ -0,0 +1,62 @@
+const { Toolkit } = require('actions-toolkit')
+const { execSync } = require('child_process')
+
+// Run your GitHub Action!
+Toolkit.run(async tools => {
+  const pkg = tools.getPackageJSON()
+  const event = tools.context.payload
+
+  const messages = event.commits.map(commit => commit.message + '\n' + commit.body)
+
+  const commitMessage = 'version bump to'
+  const isVersionBump = messages.map(message => message.toLowerCase().includes(commitMessage)).includes(true)
+  if (isVersionBump) {
+    tools.exit.success('No action necessary!')
+    return
+  }
+
+  let version = 'patch'
+  if (messages.map(message => message.toLowerCase().includes('breaking')).includes(true)) {
+    version = 'major'
+  } else if (messages.map(message => message.toLowerCase().includes('feature')).includes(true)) {
+    version = 'minor'
+  }
+  
+  try {
+    const current = pkg.version.toString()
+    // set git user
+    await tools.runInWorkspace('git', ['config', 'user.name', '"mtgatool"'])
+    await tools.runInWorkspace('git', ['config', 'user.email', '"mtgatool@gmail.com"'])
+
+    const currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1]
+    console.log('currentBranch:', currentBranch)
+
+    // do it in the current checked out github branch (DETACHED HEAD)
+    // important for further usage of the package.json version
+    await tools.runInWorkspace('npm',
+      ['version', '--allow-same-version=true', '--git-tag-version=false', current])
+    console.log('current:', current, '/', 'version:', version)
+    let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
+    await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
+
+    // now go to the actual branch to perform the same versioning
+    await tools.runInWorkspace('git', ['checkout', currentBranch])
+    await tools.runInWorkspace('npm',
+      ['version', '--allow-same-version=true', '--git-tag-version=false', current])
+    console.log('current:', current, '/', 'version:', version)
+    newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim()
+    newVersion = `${process.env['INPUT_TAG-PREFIX']}${newVersion}`
+    console.log('new version:', newVersion)
+    await tools.runInWorkspace('git', ['commit', '-a', '-m', `"ci: ${commitMessage} ${newVersion}"`])
+
+    const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+    // console.log(Buffer.from(remoteRepo).toString('base64'))
+    await tools.runInWorkspace('git', ['tag', newVersion])
+    await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags'])
+    await tools.runInWorkspace('git', ['push', remoteRepo, '--tags'])
+  } catch (e) {
+    tools.log.fatal(e)
+    tools.exit.failure('Failed to bump version')
+  }
+  tools.exit.success('Version bumped!')
+})
