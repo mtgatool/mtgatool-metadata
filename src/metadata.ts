91a60e72571eab0a7b1ae4c41234c9166e70ccad
Manwe <manuel.etchegaray7@gmail.com>
2022-08-30 11:28:57 -0300

    autobump vip channel

--- src/metadata.ts
+++ src/metadata.ts
@@ -28,7 +28,7 @@ if (!fs.existsSync(VersionDIr)) {
 
 console.log("Begin Metadata fetch.");
 
-getArenaVersion("Live")
+getArenaVersion("VIP")
   .then(getManifestFiles)
   .then(getRanksData)
   .then(getScryfallCards)
