841f588c89dd244e45bfd987916b3f86ecfdc108
Manwe <manuel.etchegaray7@gmail.com>
2022-11-03 23:21:40 -0300

    Update readExternalJson.ts

--- src/readExternalJson.ts
+++ src/readExternalJson.ts
@@ -1,19 +1,19 @@
 import path from "path";
 import fs from "fs";
-import stripComments from "strip-json-comments";
+// import stripComments from "strip-json-comments";
 import { APPDATA, EXTERNAL } from "./metadata-constants";
 
 export default function readExternalJson(filename: string): any {
   const file = path.join(APPDATA, EXTERNAL, filename);
   //JSON.parse(fs.readFileSync(file));
   const filestr = fs.readFileSync(file) + "";
-  const str = stripComments(filestr);
+  // const str = stripComments(filestr);
   try {
-    const json = JSON.parse(`{"value": ${str}}`);
+    const json = JSON.parse(`{"value": ${filestr}}`);
     return json.value;
   } catch (e) {
     console.log(filename);
-    console.log(str);
+    console.log(filestr);
   }
   return "";
 }
